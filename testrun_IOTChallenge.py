c3e394cc8465e0c6ead12e21ccbba6e0e4fa7083
edakasu <kaustav.das.sharma@ericsson.com>
2019-11-04 15:31:56 -0500

    Completed algorithm

--- testrun_IOTChallenge.py
+++ testrun_IOTChallenge.py
@@ -3,6 +3,9 @@ from google.transit import gtfs_realtime_pb2
 import time
 from datetime import datetime
 import math
+import sys
+
+# Calling the STM API to request for bus location information and bus stop location
 
 feed_trip_update = gtfs_realtime_pb2.FeedMessage()
 feed_vehicle_position = gtfs_realtime_pb2.FeedMessage()
@@ -19,6 +22,8 @@ response_trip_update = requests.request("POST", url_trip_updates, headers=header
 response_vehicle_Positions = requests.request("POST", url_vehicle_position, headers=headers)
 feed_trip_update.ParseFromString(response_trip_update.content)
 feed_vehicle_position.ParseFromString(response_vehicle_Positions.content)
+
+# Change the following to True/False in order to print /or not print
 
 show1 = False # entire vehicle_position list
 show2 = False  # entire trip_update list
@@ -47,56 +52,43 @@ if show6:
     for bus in feed_trip_update.entity:
         print(bus.trip_update.trip.route_id)
 
+# 70 -> 55788, 177 -> 55871, 213 -> 55959 
+# Function which finds the lowest wait time of a given bus at a given bus stop, and then uses the trip_id of that to print its location.
+def estimated_time_of_arrival_and_location(busNumber, stopID):
+    lowest_wait_time = sys.maxint 
+    lowest_trip_ID = "" 
+    
+    # Goes through all bus' stop information and finds the one which goes to requested stopID, and finds the lowest wait time from all of these. 
+    for bus in feed_trip_update.entity:
+        if bus.trip_update.trip.route_id == busNumber:
+            for stop in bus.trip_update.stop_time_update:
+                if stop.stop_id == stopID:
+                    tempTime = stop.departure.time
+                    if tempTime < lowest_wait_time:
+                        lowest_wait_time = tempTime
+                        lowest_trip_ID = bus.trip_update.trip.trip_id # Stores the trip ID of that specific bus with the lowest wait time
+                        
+    print("Expected arrival in unix time is: {}".format(lowest_wait_time))
+    expectedArrival = lowest_wait_time 
+    currentTime = int(time.time()) 
+    seconds = expectedArrival - currentTime
+    minutes = seconds/60
+    expectedArrival = datetime.utcfromtimestamp(expectedArrival).strftime('%Y-%m-%d %H:%M:%S') # Needs to be changed into local time
+    print("Expected arrival for bus {} in UTC: {}".format(busNumber,expectedArrival))
+    print("ETA: {} seconds".format(seconds))
+    print("ETA: {:.1f} minutes".format(minutes))
 
-lowest_wait_times = [2147483647, 2147483647, 2147483647]
-
-
-# ETA algo. 
-for bus in feed_trip_update.entity:
-    if bus.trip_update.trip.route_id == '70':
-        for stop in bus.trip_update.stop_time_update:
-            if stop.stop_id == "55788" and int(stop.arrival.time) > 0:
-                tempTime = stop.arrival.time
-                if tempTime < lowest_wait_times[0]:
-                    lowest_wait_times[0] = tempTime
-                    
-    """if bus.trip_update.trip.route_id == '177':
-        for stop in bus.trip_update.stop_time_update:
-            if stop.stop_id == "55871" and int(stop.arrival.time) > 0:
-                tempTime = stop.arrival.time
-                if tempTime < lowest_wait_times[1]:
-                    lowest_wait_times[1] = tempTime
-
-    if bus.trip_update.trip.route_id == '213':
-        for stop in bus.trip_update.stop_time_update:
-            if stop.stop_id == "52610" and int(stop.arrival.time) > 0:
-                tempTime = stop.arrival.time
-                if tempTime < lowest_wait_times[2]:
-                    lowest_wait_times[2] = tempTime """
-
-
-expectedArrival = lowest_wait_times[0] #change index for specific bus
-currentTime = int(time.time())
-seconds = expectedArrival - currentTime
-minutes = seconds/60
-expectedArrival = datetime.utcfromtimestamp(expectedArrival).strftime('%Y-%m-%d %H:%M:%S')
-print("Expected arrival for bus 70 in UTC: {}".format(expectedArrival))
-print("ETA: {} seconds".format(seconds))
-print("ETA: {:.1f} minutes".format(minutes)) 
-
-# Locations algo
-actual_shortest_distance = 1
-busIndex = 0
-for bus in feed_vehicle_position.entity:
+    actual_shortest_distance = sys.maxint
+    busIndex = 0
     count = 0
-    if bus.vehicle.trip.route_id == "70" and int(bus.vehicle.current_stop_sequence) < 15:
-        temp_shortest_distance = math.sqrt((45.491631 - bus.vehicle.position.latitude) ** 2 + (-73.727036 - bus.vehicle.position.longitude) ** 2)
-        if temp_shortest_distance<actual_shortest_distance:
-            actual_shortest_distance = temp_shortest_distance
-            busIndex = count
-    count=count+1
-        
-print("Bus number 70 is at x-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.latitude))
-print("Bus number 70 is at y-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.longitude))
 
-#stop_sequence_for_177 is 7
+    for bus in feed_vehicle_position.entity:
+
+        if bus.vehicle.trip.trip_id == lowest_trip_ID:
+            print("Trip ID is Valid")
+            busIndex = count    
+        count=count+1 
+            
+    print("Bus number 70 is at x-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.latitude))
+    print("Bus number 70 is at y-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.longitude))
+    
