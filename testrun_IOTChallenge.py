50a8af02b0013c925b123ae2ccde9363bcb3467f
William M-L <w.morin.laberge@gmail.com>
2019-11-05 18:47:40 -0500

    Using a dictionary instead of multiple function calls. Not too good for testing, but more efficient

--- testrun_IOTChallenge.py
+++ testrun_IOTChallenge.py
@@ -25,9 +25,9 @@ feed_vehicle_position.ParseFromString(response_vehicle_Positions.content)
 
 # Change the following to True/False in order to print /or not print
 
-show1 = False # entire vehicle_position list
+show1 = False  # entire vehicle_position list
 show2 = False  # entire trip_update list
-show3 = False # first entry in vehicle_position
+show3 = False  # first entry in vehicle_position
 show4 = False  # first entry in trip_update
 show5 = False  # all route_ids in vehicle positions
 show6 = False  # all route_ids in trip updates
@@ -52,33 +52,42 @@ if show6:
     for bus in feed_trip_update.entity:
         print(bus.trip_update.trip.route_id)
 
-# 70 -> 55788, 177 -> 55871, 213 -> 55959 
-# Function which finds the lowest wait time of a given bus at a given bus stop, and then uses the trip_id of that to print its location.
-def estimated_time_of_arrival_and_location(busNumber, stopID):
-    lowest_wait_time = sys.maxint 
-    lowest_trip_ID = "" 
-    
-    # Goes through all bus' stop information and finds the one which goes to requested stopID, and finds the lowest wait time from all of these. 
+relevantBuses = {'70': '55788', '177': '55871', '213': '55959'}
+
+
+# 70 -> 55788, 177 -> 55871, 213 -> 55959
+# Function which finds the lowest wait time of a given bus at a given bus stop,
+# and then uses the trip_id of that to print its location.
+def estimated_time_of_arrival_and_location():
+    lowest_wait_time = 99999999999999999999999999999
+    lowest_trip_ID = ""
+
+    # busNumber = str(busNumber)
+    # stopID = str(stopID)
+
+    # Goes through all bus' stop information and finds the one which goes to requested stopID, and finds the lowest wait time from all of these.
     for bus in feed_trip_update.entity:
-        if bus.trip_update.trip.route_id == busNumber:
+        for busNumber in (busNumber for busNumber in relevantBuses if bus.trip_update.trip.route_id == busNumber):
+            print(busNumber)
             for stop in bus.trip_update.stop_time_update:
-                if stop.stop_id == stopID:
+                if stop.stop_id == relevantBuses[busNumber]:
                     tempTime = stop.departure.time
                     if tempTime < lowest_wait_time:
                         lowest_wait_time = tempTime
-                        lowest_trip_ID = bus.trip_update.trip.trip_id # Stores the trip ID of that specific bus with the lowest wait time
-                        
+                        lowest_trip_ID = bus.trip_update.trip.trip_id  # Stores the trip ID of that specific bus with the lowest wait time
+
     print("Expected arrival in unix time is: {}".format(lowest_wait_time))
-    expectedArrival = lowest_wait_time 
-    currentTime = int(time.time()) 
+    expectedArrival = lowest_wait_time
+    currentTime = int(time.time())
     seconds = expectedArrival - currentTime
-    minutes = seconds/60
-    expectedArrival = datetime.utcfromtimestamp(expectedArrival).strftime('%Y-%m-%d %H:%M:%S') # Needs to be changed into local time
-    print("Expected arrival for bus {} in UTC: {}".format(busNumber,expectedArrival))
+    minutes = seconds / 60
+    expectedArrival = datetime.utcfromtimestamp(expectedArrival).strftime(
+        '%Y-%m-%d %H:%M:%S')  # Needs to be changed into local time
+    print("Expected arrival for bus {} in UTC: {}".format(busNumber, expectedArrival))
     print("ETA: {} seconds".format(seconds))
     print("ETA: {:.1f} minutes".format(minutes))
 
-    actual_shortest_distance = sys.maxint
+    actual_shortest_distance = 99999999999999999999999999999
     busIndex = 0
     count = 0
 
@@ -86,9 +95,10 @@ def estimated_time_of_arrival_and_location(busNumber, stopID):
 
         if bus.vehicle.trip.trip_id == lowest_trip_ID:
             print("Trip ID is Valid")
-            busIndex = count    
-        count=count+1 
-            
+            busIndex = count
+        count = count + 1
+
     print("Bus number 70 is at x-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.latitude))
     print("Bus number 70 is at y-coord: {}".format(feed_vehicle_position.entity[busIndex].vehicle.position.longitude))
-    
+
+estimated_time_of_arrival_and_location()
