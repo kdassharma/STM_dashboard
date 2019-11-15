a87068b10372d02f845f3325b84a864fb8f30a49
William M-L <w.morin.laberge@gmail.com>
2019-11-14 20:44:27 -0500

    restructuring

--- testrun_IOTChallenge.py
+++ testrun_IOTChallenge.py
@@ -24,28 +24,6 @@ class incomingBus:
                 return [bus.vehicle.position.latitude, bus.vehicle.position.longitude]
 
 
-# Calling the STM API to request for bus location information and bus stop location
-feed_trip_update = gtfs_realtime_pb2.FeedMessage()
-feed_vehicle_position = gtfs_realtime_pb2.FeedMessage()
-url_trip_updates = "https://api.stm.info/pub/od/gtfs-rt/ic/v1/tripUpdates"
-url_vehicle_position = "https://api.stm.info/pub/od/gtfs-rt/ic/v1/vehiclePositions"
-payload = ""
-
-headers = {
-    'origin': "mon.domain.xyz",
-    'apikey': "l7xxfec365f2dad04b158e1583f6fc4a23cf"
-}
-
-response_trip_update = requests.request("POST", url_trip_updates, headers=headers)
-response_vehicle_Positions = requests.request("POST", url_vehicle_position, headers=headers)
-feed_trip_update.ParseFromString(response_trip_update.content)
-feed_vehicle_position.ParseFromString(response_vehicle_Positions.content)
-
-relevantBuses = {'70': None, '177': None, '213': None}
-relevantBusesWithStops = {'70': '55788', '177': '55871', '213': '55959'}
-incomingBuses = []
-
-
 def markEarliestBuses():
     lowest_wait_time = 2147483647
     earliestBuses = {}
@@ -79,12 +57,33 @@ def estimated_time_of_arrival_and_location():
                         incomingBuses.append(incomingBus(bus, stop.stop_id, stop.departure.time, busNumber,
                                                          bus.trip_update.trip.trip_id))
 
+if __name__ == "__main__":
+    # Calling the STM API to request for bus location information and bus stop location
+    feed_trip_update = gtfs_realtime_pb2.FeedMessage()
+    feed_vehicle_position = gtfs_realtime_pb2.FeedMessage()
+    url_trip_updates = "https://api.stm.info/pub/od/gtfs-rt/ic/v1/tripUpdates"
+    url_vehicle_position = "https://api.stm.info/pub/od/gtfs-rt/ic/v1/vehiclePositions"
+    payload = ""
 
-estimated_time_of_arrival_and_location()
-earliestBuses = markEarliestBuses()
+    headers = {
+        'origin': "mon.domain.xyz",
+        'apikey': "l7xxfec365f2dad04b158e1583f6fc4a23cf"
+    }
 
-for bus in incomingBuses:
-    print("Bus Number: " + bus.busNumber)
-    print("Bus ETA: " + str(bus.ETA))
-    print("isEarliest?: " + str(bus.isEarliest))
-    print("=================")
+    response_trip_update = requests.request("POST", url_trip_updates, headers=headers)
+    response_vehicle_Positions = requests.request("POST", url_vehicle_position, headers=headers)
+    feed_trip_update.ParseFromString(response_trip_update.content)
+    feed_vehicle_position.ParseFromString(response_vehicle_Positions.content)
+
+    relevantBuses = {'70': None, '177': None, '213': None}
+    relevantBusesWithStops = {'70': '55788', '177': '55871', '213': '55959'}
+    incomingBuses = []
+
+    estimated_time_of_arrival_and_location()
+    earliestBuses = markEarliestBuses()
+
+    for bus in incomingBuses:
+        print("Bus Number: " + bus.busNumber)
+        print("Bus ETA: " + str(bus.ETA))
+        print("isEarliest?: " + str(bus.isEarliest))
+        print("=================")
